28b566e13b64c919992495f92c12932840702278
Arunav <arunavsharma11@gmail.com>
2019-10-30 10:23:02 +0530

    added attendance recorder

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Deepak Tiwari\Desktop\HR-Management-and-Geo-Attendance-System-master"
+export "FLUTTER_ROOT=E:\Movies\course\Compressed\flutter_windows_v1.9.1+hotfix.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Arunav\AndroidStudioProjects\HR-Management-and-Geo-Attendance-System"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\Movies\course\Compressed\flutter_windows_v1.9.1+hotfix.4-stable\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
