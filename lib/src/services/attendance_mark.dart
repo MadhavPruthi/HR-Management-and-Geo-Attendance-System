36cb2dc0dc7f48bd6758d24678f94779da7a02c7
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-11-16 19:27:46 +0530

    GeoFence Working Latest

--- lib/src/services/attendance_mark.dart
+++ lib/src/services/attendance_mark.dart
@@ -4,17 +4,31 @@ import 'package:location/location.dart';
 
 import 'geofence.dart';
 
+void showDialogTemplate(BuildContext context, String title, String subtitle) {
+  showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(subtitle),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Great!'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
 void markInAttendance(
     BuildContext context, Office office, LocationData currentPosition) {
-  print(GeoFenceClass.geofenceState);
   if (GeoFenceClass.geofenceState == "GeofenceEvent.dwell" ||
       GeoFenceClass.geofenceState == "GeofenceEvent.enter") {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return Text("Button Pressed");
-      },
-    );
+    showDialogTemplate(
+        context, "Attendance Info", "Marked ${GeoFenceClass.geofenceState}");
   }
 }
