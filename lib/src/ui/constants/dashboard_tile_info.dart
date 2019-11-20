20102e6e8e5d986e4612092a7e9fb6c486fb7d01
Arunav <arunavsharma11@gmail.com>
2019-11-20 14:52:03 +0530

    leave form basic implemented plz don't cry

--- lib/src/ui/constants/dashboard_tile_info.dart
+++ lib/src/ui/constants/dashboard_tile_info.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import '../pages/leave_application.dart';
 import '../pages/attendance_recorder.dart';
 import '../pages/attendance_summary.dart';
 
@@ -23,6 +23,11 @@ void attendanceRecorderCallback(BuildContext context, FirebaseUser user) {
           builder: (context) => AttendanceRecorderWidget(user: user)));
 }
 
+void leaveApplicationCallback(BuildContext context,FirebaseUser user)
+{
+  Navigator.push(context,
+  MaterialPageRoute(builder: (context) => LeaveApplicationWidget(title: "leAVE", user: user,)));
+}
 List<List> infoAboutTiles = [
   [
     Icons.pin_drop,
@@ -36,6 +41,6 @@ List<List> infoAboutTiles = [
     "Check your previous record",
     attendanceSummaryCallback
   ],
-  [Icons.time_to_leave, "Leaves Application/Withdrawal", "Management", null],
+  [Icons.time_to_leave, "Leaves Application/Withdrawal", "Management", leaveApplicationCallback],
   [Icons.timeline, "Leaves Status", "Check pending status of leaves", null],
 ];
