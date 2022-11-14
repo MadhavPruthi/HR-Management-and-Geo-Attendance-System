49cc3d28226bb0394fa8f248bda9bd1900f2d840
harpreet-applify <happy_san@protonmail.com>
2022-11-14 22:38:33 +0530

    refactor: Move GeoFencingService into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:geo_attendance_system/src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:geo_attendance_system/src/services/geofencing.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(App());
+  runApp(
+    GeoFencing(
+      service: GeoFencingService(),
+      child: App(),
+    ),
+  );
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:geo_attendance_system/src/services/authentication.dart';
-import 'package:geo_attendance_system/src/services/geofencing.dart';
 
 import 'package:geo_attendance_system/src/ui/pages/splash_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GeoFencing(
-      service: GeoFencingService(),
-      child: MaterialApp(
-        theme: ThemeData.light(),
-        home: Scaffold(
-          body: SplashScreenWidget(
-            auth: new Auth(),
-          ),
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(
+        body: SplashScreenWidget(
+          auth: new Auth(),
         ),
-        debugShowCheckedModeBanner: false,
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
